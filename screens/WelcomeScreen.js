0dea4f33f91ac9db3119c6657b558e6b1a68df90
Butsoap <soravichn@gmail.com>
2023-12-18 16:30:48 +0700

    strart first react native project

---
+++ screens/WelcomeScreen.js
@@ -0,0 +1,27 @@
+import { View, Text, Image, TouchableOpacity } from 'react-native'
+import React from 'react'
+import ScreennWrapper from '../components/screennWrapper'
+import { colors } from '../theme'
+import { useNavigation } from '@react-navigation/native'
+
+export default function WelcomeScreen() {
+  const navigation = useNavigation();
+  return (
+    <ScreennWrapper>
+      <View className="h-full flex justify-around">
+        <View className="flex-row justify-center mt-10">
+          <Image source={require('../assets/images/welcome.gif')} className="h-96 w-96 shadow" />
+        </View>
+        <View className="mx-5 mb-20">
+          <Text className={`text-center font-bold text-4xl ${colors.heading} mb-10`}>Expensify</Text>
+          <TouchableOpacity onPress={() => navigation.navigate('SignIn')} className="shadow p-3 rounded-full mb-5" style={{ backgroundColor: colors.button }}>
+            <Text className="text-center text-white text-lg font-bold">Sign In</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => navigation.navigate('SignUp')} className="shadow p-3 rounded-full" style={{ backgroundColor: colors.button }}>
+            <Text className="text-center text-white text-lg font-bold">Sign Up</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ScreennWrapper>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.{js,jsx,ts,tsx}", "./screens/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'heading': '#374151',
+        'button': '#50C878',
+      }
+    },
+  },
+}
+
